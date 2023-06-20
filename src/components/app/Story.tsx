c6f581414cdc31c72a91dc7f6b943a9414149b15
Keeghan McGarry <keeghan_m@live.com>
2023-06-20 21:37:29 +0100

    Better titles and layout

--- src/components/app/Story.tsx
+++ src/components/app/Story.tsx
@@ -16,7 +16,6 @@ export default function Story(props: storyProps) {
   const sidebarItems = [
     { name: "Arcs", screenId: "arcs" },
     { name: "Collections", screenId: "collections" },
-    { name: "Things", screenId: "things" },
   ]
 
   const arcs = Arcs
@@ -33,6 +32,12 @@ export default function Story(props: storyProps) {
     )
   }
 
+  const toTitleCase = (str: string) => {
+    return str.replace(/\w\S*/g, function (txt: string) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    })
+  }
+
   return (
     <>
       <ul class="sidebar">
@@ -44,16 +49,29 @@ export default function Story(props: storyProps) {
         </li>
       </ul>
       <main>
-        {screen() == "arcs" ? (
-          <>
-            <div class="screenTitle">
-              <h1>Arcs</h1>
-              <button class="newArc">Add New</button>
-            </div>
-            <div class="arcContainer">
+        <div class="screenTitle">
+          <h1>{toTitleCase(screen())}</h1>
+          <button class="newButton">Add New</button>
+        </div>
+        <div class="cardContainer">
+          {screen() == "arcs" ? (
+            <>
               {arcs.map((arc) => (
-                <div class="arcCard">
-                  <p class="arcName">{arc.name}</p>
+                <div class="card">
+                  <p class="cardName" onclick={() => alert(arc.name)}>
+                    {arc.name}{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h7v2H5v14h14v-7h2v7q0 .825-.588 1.413T19 21H5Zm4.7-5.3l-1.4-1.4L17.6 5H14V3h7v7h-2V6.4l-9.3 9.3Z"
+                      />
+                    </svg>
+                  </p>
                   <p class="arcHook">
                     {truncate(arc.information["hook"], 80, true)}
                   </p>
@@ -65,29 +83,13 @@ export default function Story(props: storyProps) {
                   </ul>
                 </div>
               ))}
-              {arcs.map((arc) => (
-                <div class="arcCard">
-                  <p class="arcName">{arc.name}</p>
-                  <p class="arcHook">
-                    {truncate(arc.information["hook"], 80, true)}
-                  </p>
-                  <p class="subArcTitle">Arcs</p>
-                  <ul class="subArcs">
-                    {arc.SubArcs.map((arc) => {
-                      return <li>{arc.name}</li>
-                    })}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          </>
-        ) : screen() == "collections" ? (
-          <p>Collections</p>
-        ) : screen() == "things" ? (
-          <p>Things</p>
-        ) : (
-          <p>Select a screen...</p>
-        )}
+            </>
+          ) : screen() == "collections" ? (
+            <p>Collections</p>
+          ) : (
+            <p>Select a screen...</p>
+          )}
+        </div>
       </main>
     </>
   )
